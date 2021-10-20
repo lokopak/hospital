efb837d17ae29a2bb85f1c134e1f3388ca7e24a7
Shoukei94 <mascia17@hotmail.com>
2021-10-20 20:38:23 +0200

    Variables persona

--- model/Persona.php
+++ model/Persona.php
@@ -2,4 +2,26 @@
 
 abstract class Persona extends Entidad
 {
+    protected $nombre;
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+    protected $apellidos;
+
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos= $apellidos;
+    }
 }
