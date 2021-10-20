813c004d022c712bb512f8e4e0ebb6b1b0930ee7
lokopak <pakpil@gmail.com>
2021-10-20 23:52:32 +0200

    Arreglado pequeño error en tabla.sql

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS 'hospital';
+CREATE DATABASE IF NOT EXISTS hospital;
 CREATE USER IF NOT EXISTS 'hospital'@'localhost' IDENTIFIED BY 'hospital';
 GRANT ALL PRIVILEGES ON hospital.* TO 'hospital'@'localhost';
 FLUSH PRIVILEGES;
