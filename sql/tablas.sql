835df37ec3d257810732823d4811dd4721ed0970
Catalin021 <07251931sg@gmail.com>
2021-10-20 19:11:35 +0200

    Update tablas.sql
    Añadida tabla pacientes

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -12,5 +12,22 @@ CREATE TABLE IF NOT EXISTS empleados (
     DNI varchar(9) NOT NULL UNIQUE,
     cargo tinyint(1) NOT NULL,
     _password varchar(512) NOT NULL,
-    INDEX empleado_dni_index (DNI)
+    INDEX empleado_dni_index (DNI),
+    INDEX empleado_nombre_apellidos_index (nombre,apellidos)
 );
+
+CREATE TABLE IF NOT EXISTS pacientes (
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(40) NOT NULL,
+    apellidos varchar(80) NOT NULL,
+    habitacion varchar(9) NOT NULL,
+    dieta tinyint(1) NOT NULL DEfAULT 0,
+    estado tinyint(1) NOT NULL,
+    INDEX paciente_nombre_apellidos_index (nombre,apellidos)
+);
+
+CREATE TABLE IF NOT EXISTS informes (
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    
+);
+
