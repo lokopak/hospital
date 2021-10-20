caef6eabef7fc8b1d3d0ae00eb067a4c71946b06
Shoukei94 <mascia17@hotmail.com>
2021-10-20 19:42:40 +0200

    Variables pacientes

--- pacientes/model/Paciente.php
+++ pacientes/model/Paciente.php
@@ -2,4 +2,65 @@
 
 class Paciente
 {
+    protected $habitacion;
+
+    public function getHabiticion()
+    {
+        return $this->habitacion;
+    }
+
+    public function setHabitacion($habitacion)
+    {
+        $this->habitacion = $habitacion;
+    }
+    protected $dieta;
+
+    public function getDieta()
+    {
+        return $this->dieta;
+    }
+
+    public function setDieta($dieta)
+    {
+        $this->dieta = $dieta;
+    }
+
+    protected $estado;
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+
+    }
+
+    protected $fechaRegistro;
+
+    public function getFechaRegistro()
+    {
+        return $this->fechaRegistro;
+    }
+
+    public function setFechaRegistro($fechaRegistro)
+    {
+        $this->fechaRegistro = $fechaRegistro;
+
+    }
+    protected $fechaSaliada;
+
+    public function getfechaSalida()
+    {
+        return $this->fechaSalida;
+    }
+
+    public function setFechaSalida($fechaSalida)
+    {
+        $this->fechaSaliada = $fechaSalida;
+
+    }
+    
 }
