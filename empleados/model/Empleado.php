cafaefdca50edafec507c8c0d5a62ce3be17dffb
Shoukei94 <mascia17@hotmail.com>
2021-10-20 19:26:15 +0200

    Variables empleados

--- empleados/model/Empleado.php
+++ empleados/model/Empleado.php
@@ -2,4 +2,40 @@
 
 abstract class Empleado
 {
+    protected $DNI;
+
+    public function getDNI()
+    {
+        return $this->DNI;
+    }
+
+    public function setDNI($DNI)
+    {
+        $this->DNI = $DNI;
+    }
+    protected $cargo;
+
+    public function getCargo()
+    {
+        return $this->cargo;
+    }
+
+    public function setCargo($cargo)
+    {
+        $this->cargo = $cargo;
+    }
+
+    protected $contraseña;
+
+    public function getContraseña()
+    {
+        return $this->contraseña;
+    }
+
+    public function setContraseña($contraseña)
+    {
+        $this->contraseña = $contraseña;
+
+    }
+
 }
