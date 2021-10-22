2843a6f6c20065214f67c8317e2bbb8b803485b9
lokopak <pakpil@gmail.com>
2021-10-22 19:34:30 +0200

    Agregados estados de pacienetes

--- pacientes/model/Paciente.php
+++ pacientes/model/Paciente.php
@@ -2,6 +2,20 @@
 require_once(__DIR__ . "/../../model/Persona.php");
 class Paciente extends Persona
 {
+    const PACIENTE_ESTADO_ALTA = 0;
+    const PACIENTE_ESTADO_FALLECIDO = 1;
+    const PACIENTE_ESTADO_PREOPERATORIO = 2;
+    const PACIENTE_ESTADO_POSTOPERATORIO = 3;
+    const PACIENTE_ESTADO_UCI = 4;
+
+    protected $estados = [
+        self::PACIENTE_ESTADO_ALTA => "alta",
+        self::PACIENTE_ESTADO_FALLECIDO => "fallecido",
+        self::PACIENTE_ESTADO_PREOPERATORIO => "pre-operatorio",
+        self::PACIENTE_ESTADO_POSTOPERATORIO => "post-operatorio",
+        self::PACIENTE_ESTADO_UCI => "uci",
+    ];
+
     protected $habitacion;
 
     public function getHabitacion()
@@ -60,5 +74,10 @@ class Paciente extends Persona
     public function setFechaSalida($fechaSalida)
     {
         $this->fechaSalida = $fechaSalida;
+    }
+
+    public static function getEstados()
+    {
+        return self::$estados;
     }
 }
