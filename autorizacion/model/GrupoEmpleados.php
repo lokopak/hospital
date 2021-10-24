5d2159f6e1c60a2c59bea03627d79048f2fde957
lokopak <pakpil@gmail.com>
2021-10-24 15:31:29 +0200

    Create GrupoEmpleados.php

---
+++ autorizacion/model/GrupoEmpleados.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Esta clase representa a un grupo de empleados y
+ * almacena y permite acceso a todos los permisos
+ * a los que tiene acceso el grupo de empleados.
+ */
+class GrupoEmpleados
+{
+}
