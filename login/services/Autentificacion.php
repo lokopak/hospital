4f7182ced16113342281d100b5c97eca275c96b8
lokopak <pakpil@gmail.com>
2021-11-05 11:12:33 +0100

    Agregado sistema inicial de autorización y autentificación

---
+++ login/services/Autentificacion.php
@@ -0,0 +1,97 @@
+<?php
+
+class Autentificacion
+{
+    /**
+     * Este variabl
+     */
+    protected $contenedor;
+
+    /**
+     * Instancia única de esta clase.
+     * 
+     * @var \Autentificacion
+     */
+    protected static $instancia;
+
+    /**
+     * Campo de la tabla de empleados usada como identificador.
+     * 
+     * @var string
+     */
+    protected $columnaIdentificacion = 'DNI';
+
+    /**
+     * Constructor.
+     * 
+     * Declarado privado para evitar crear objetos de esta clase.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Devuelve la única instancia de esta clase.
+     * En caso de no haberse creado ya, esta será creada.
+     * 
+     * @return Autentificacion
+     */
+    public static function getInstancia()
+    {
+        // Si no está creada, la creamos primero.
+        if (!self::$instancia) {
+            self::$instancia = new Autentificacion();
+        }
+
+        // Devolvemos la instancia.
+        return self::$instancia;
+    }
+
+    /**
+     * Realiza un intento de login
+     */
+    public function autentificar($username, $password)
+    {
+        // Recordamos que cada acción que tenga que ver con la base de datos, debe ir en un bloque try-catch para evitar filtraciones de datos.
+        try {
+            // Esta tabla solo nos hace falta en esta función, al menos de momento.
+            require_once __DIR__ . "/../../empleados/model/TablaEmpleado.php";
+            require_once __DIR__ . "/../../empleados/model/Empleado.php";
+
+            $tablaEmpleados = new TablaEmpleado();
+
+            /**
+             * Extraemos los datos necesarios para realizar el login. Necesitamos el cargo para comprobar que el usuario está activado.
+             */
+            $usuario = $tablaEmpleados->buscarUno($username, $this->columnaIdentificacion, ['id', 'DNI', 'userPassword', 'cargo']);
+
+            // Si se ha producido un error en la base de datos, simplemente devolvemos el error.
+            if ($usuario instanceof AppError) {
+                return $usuario;
+            }
+
+            // Si el usuario no existe en la base de datos, no se puede hacer nada.
+            if (null === $usuario || false === $usuario) {
+                // Devolvemos este mensaje para evitar dar pistas de si el usuario existe o no.
+                return [false, 'El usuario o contraseña incorrecto'];
+            }
+
+            // Comprobamos que el usuario está activado y tiene acceso a la aplicación.
+            if ($usuario['cargo'] === Empleado::CARGO_EMPLEADO_INACTIVO) {
+                // Alguna ha liado el empleado y no tiene acceso... se lo decimos para que sienta la presión...
+                return [false, 'Este usuario no tiene acceso actualmente. Pongase en contacto con los administradores del sistema.'];
+            }
+
+            // Comprobamos que las contraseñas concuerdan.
+            if (password_verify($password, $usuario['userPassword'])) {
+                // Login correcto, adelante!!!
+                return [true, 'Autentificación correcta'];
+            }
+
+            // De nuevo, si la contraseña no es correcta, mostramos el mismo mensaje para no dar pistas.
+            return [false, 'El usuario o contraseña incorrecto'];
+        } catch (Exception $e) {
+            return new AppError("Error inesperado", "Se ha producido un error grave y no se ha podido llevar a cabo la acción solicitada.");
+        }
+    }
+}
---
+++ login/services/Autorizacion.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once __DIR__ . "/Autentificacion.php";
+
+/**
+ * Esta clase se encarga de gestionar y realizar
+ * los intentos de login y logut de la aplicación.
+ */
+class Autorizacion
+{
+    /**
+     * Servicio que se encargará de realizar las autentificaciones.
+     * 
+     * @var \Autentificacion
+     */
+    protected $autentificador;
+
+    /**
+     * Instancia única de esta clase.
+     * 
+     * @var \Autorizacion
+     */
+    protected static $instancia;
+
+    /**
+     * Constructor.
+     * 
+     * Declarado privado para evitar crear objetos de esta clase.
+     */
+    private function __construct()
+    {
+        // Obtenemos el servicio encargado de llevar a cabo la autentificación.
+        $this->setAutentificador();
+    }
+
+    /**
+     * Devuelve la única instancia de esta clase.
+     * En caso de no haberse creado ya, esta será creada.
+     * 
+     * @return Autorizacion
+     */
+    public static function getInstancia()
+    {
+        // Si no está creada, la creamos primero.
+        if (!self::$instancia) {
+            self::$instancia = new Autorizacion();
+        }
+
+        // Devolvemos la instancia.
+        return self::$instancia;
+    }
+
+    /**
+     * Fija el autentificador para realizar el login.
+     * 
+     * @return void
+     */
+    public function setAutentificador()
+    {
+        // Si ya está fijado el autentificador, no es necesario volver a hacerlo.
+        if (!$this->autentificador) {
+            $this->autentificador = Autentificacion::getInstancia();
+        }
+    }
+
+    /**
+     * Realiza un intento de login.
+     * 
+     * @return mixed
+     */
+    public function login($username, $password)
+    {
+        // TODO: COMPROBAR SI EL USUARIO YA ESTÁ LOGEADO PARA EVITAR DOBLE LOGIN.
+
+        // Devolvemos el resultado optenido de la autentificación del usuario.
+        return $this->autentificador->autentificar($username, $password);
+    }
+
+    /**
+     * Realiza un intento de logout
+     * 
+     * @return
+     */
+    public static function logout()
+    {
+        // TODO: Optimizar
+        require_once(__DIR__ . "/../../services/Sesion.php");
+
+        Sesion::desconectar();
+    }
+}
