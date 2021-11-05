12c536a1fd511ad1c5c04838f70d8d7d8305d21c
lokopak <pakpil@gmail.com>
2021-11-05 11:38:21 +0100

    Update Sesion.php

--- services/Sesion.php
+++ services/Sesion.php
@@ -2,10 +2,43 @@
 class Sesion
 {
 
-    public static function sesionIniciada()
+    /**
+     * Instancia única de esta clase.
+     * 
+     * @var \Autentificacion
+     */
+    protected static $instancia;
+
+    /**
+     * Constructor.
+     * 
+     * Declarado privado para evitar crear objetos de esta clase.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Devuelve la única instancia de esta clase.
+     * En caso de no haberse creado ya, esta será creada.
+     * 
+     * @return Sesion
+     */
+    public static function getInstancia()
+    {
+        // Si no está creada, la creamos primero.
+        if (!self::$instancia) {
+            self::$instancia = new Sesion();
+        }
+
+        // Devolvemos la instancia.
+        return self::$instancia;
+    }
+
+    public function sesionIniciada()
     {
         // Initialize the session
-        if (static::sesionEmpezada() == FALSE) {
+        if ($this->sesionEmpezada() == FALSE) {
             session_start();
         }
 
@@ -15,11 +48,11 @@ class Sesion
             exit;
         }
     }
-    private static function sesionEmpezada()
+    private function sesionEmpezada()
     {
         return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
     }
-    public static function iniciarSesion($loggedin, $id, $username)
+    public function iniciarSesion($loggedin, $id, $username)
     {
         session_set_cookie_params(3600);
         session_start();
@@ -28,7 +61,7 @@ class Sesion
         $_SESSION["id"] = $id;
         $_SESSION["username"] = $username;
     }
-    public static function desconectar()
+    public function desconectar()
     {
         session_start();
         $_SESSION = array();
