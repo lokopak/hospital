bc6eab41d22513f95bc89ef4164820be4e0106f4
lokopak <pakpil@gmail.com>
2021-11-16 16:21:00 +0100

    Update ControladorBuscar.php

--- buscar/controller/ControladorBuscar.php
+++ buscar/controller/ControladorBuscar.php
@@ -5,12 +5,14 @@ require_once(__DIR__ . "/../../services/Peticion.php");
 require_once(__DIR__ . "/../../empleados/model/TablaEmpleado.php");
 require_once(__DIR__ . "/../../pacientes/model/TablaPaciente.php");
 require_once(__DIR__ . "/../../services/AppError.php");
-require_once __DIR__. "/../../services/paginador/Paginador.php";
+require_once __DIR__ . "/../../services/paginador/Paginador.php";
 
 
-class ControladorBuscar extends Controlador{
+class ControladorBuscar extends Controlador
+{
 
-    public function buscar(){
+    public function buscar()
+    {
         if (Peticion::getInstancia()->esPost()) {
             // Recoger del post el dato a bucar
 
@@ -23,7 +25,7 @@ class ControladorBuscar extends Controlador{
 
             if ($seccion === 'pacientes') {
                 $tabla = new TablaPaciente();
-                $colunmnas= ['id', 'nombre', 'apellidos', 'habitacion', 'dieta', 'estado', 'fechaRegistro', 'fechaSalida', 'fechaNacimiento'];
+                $colunmnas = ['id', 'nombre', 'apellidos', 'habitacion', 'dieta', 'estado', 'fechaRegistro', 'fechaSalida', 'fechaNacimiento'];
 
                 $elementos = $tabla->buscarTodos($colunmnas, ['where' => ['nombre' => $valor, 'apellidos' => $valor]]);
             } elseif ($seccion === 'empleados') {
@@ -31,8 +33,7 @@ class ControladorBuscar extends Controlador{
                 $colunmnas = ['id', 'nombre', 'apellidos', 'DNI', 'cargo'];
 
                 $elementos = $tabla->buscarTodos($colunmnas, ['where' => ['nombre' => $valor, 'apellidos' => $valor]]);
-            }
-            else {
+            } else {
                 return (new AppError('Petición no válida', 'Selecciona Paciente o Empleado para poder continuar'))->mostrarError();
             }
 
@@ -42,13 +43,12 @@ class ControladorBuscar extends Controlador{
             }
 
             if ($seccion === 'pacientes') {
-                $pacientes = $elementos;
+                $pacientes = new Paginador($elementos, 1, 20);
                 $contenido = __DIR__ . "/../../pacientes/view/index.phtml";
             } elseif ($seccion === 'empleados') {
                 $elementos = new Paginador($elementos, 1, 20);
                 $contenido = __DIR__ . "/../../empleados/view/index.phtml";
             } else {
-                print_r($elementos);
             }
 
             return require __DIR__ . "/../../view/pagina.phtml";
