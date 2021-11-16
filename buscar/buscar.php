524a29ee0c15e4b16209a75e7a3859a77605792b
Catalin021 <07251931sg@gmail.com>
2021-11-16 16:08:59 +0100

    buscar

--- buscar/buscar.php
+++ buscar/buscar.php
@@ -1,45 +1,10 @@
 <?php
-require_once(__DIR__ . "/../model/Tabla.php");
-require_once(__DIR__ . "/../services/Peticion.php");
-require_once(__DIR__ . "/../empleados/model/TablaEmpleado.php");
-require_once(__DIR__. "/../services/AppError.php");
-if (Peticion::getInstancia()->esPost()) {
-    // Recoger del post el dato a bucar
 
-    $tabla = new TablaEmpleado();
+require_once __DIR__ . "/controller/ControladorBuscar.php";
 
-    $seccion = Peticion::getInstancia()->fromPost('seccion');
-    $valor = Peticion::getInstancia()->fromPost('valor');
+$contolador = new ControladorBuscar();
 
-    if (null === $seccion || empty($seccion)) {
-        return (new AppError('Petición invalida', 'La petición no incluye los datos necesarios para ser procesada.'))->mostrarError();
-    }
-
-    $query = sprintf("SELECT * FROM %s ", $seccion);
-    if (null !== $valor) {        
-        $query .= sprintf(' WHERE nombre = \'%1$s\' OR apellidos = \'%1$s\'', $valor);
-    }
-
-    $elementos = $tabla->query($query);
-
-    if ($elementos instanceof AppError) {
-        return $elementos->mostrarError();
-    }
-
-}
-
-if ($seccion === 'pacientes'){
-    $elementos = __DIR__ . "/../pacientes/view/index.phtml";
-}
-elseif ($seccion === 'empleados'){
-    $elementos = __DIR__. "/../empleados/view/index.phtml";
-}
-
-
-
-return require(__DIR__ . "/../view/pagina.phtml");
-
-
+return $contolador->buscar();
 
 
 
---
+++ buscar/controller/ControladorBuscar.php
@@ -0,0 +1,57 @@
+<?php
+require_once __DIR__ . "/../../controller/Controlador.php";
+require_once(__DIR__ . "/../../model/Tabla.php");
+require_once(__DIR__ . "/../../services/Peticion.php");
+require_once(__DIR__ . "/../../empleados/model/TablaEmpleado.php");
+require_once(__DIR__ . "/../../pacientes/model/TablaPaciente.php");
+require_once(__DIR__ . "/../../services/AppError.php");
+require_once __DIR__. "/../../services/paginador/Paginador.php";
+
+
+class ControladorBuscar extends Controlador{
+
+    public function buscar(){
+        if (Peticion::getInstancia()->esPost()) {
+            // Recoger del post el dato a bucar
+
+            $seccion = Peticion::getInstancia()->fromPost('seccion');
+            $valor = Peticion::getInstancia()->fromPost('valor');
+
+            if (null === $seccion || empty($seccion)) {
+                return (new AppError('Petición invalida', 'La petición no incluye los datos necesarios para ser procesada.'))->mostrarError();
+            }
+
+            if ($seccion === 'pacientes') {
+                $tabla = new TablaPaciente();
+                $colunmnas= ['id', 'nombre', 'apellidos', 'habitacion', 'dieta', 'estado', 'fechaRegistro', 'fechaSalida', 'fechaNacimiento'];
+
+                $elementos = $tabla->buscarTodos($colunmnas, ['where' => ['nombre' => $valor, 'apellidos' => $valor]]);
+            } elseif ($seccion === 'empleados') {
+                $tabla = new TablaEmpleado();
+                $colunmnas = ['id', 'nombre', 'apellidos', 'DNI', 'cargo'];
+
+                $elementos = $tabla->buscarTodos($colunmnas, ['where' => ['nombre' => $valor, 'apellidos' => $valor]]);
+            }
+            else {
+                return (new AppError('Petición no válida', 'Selecciona Paciente o Empleado para poder continuar'))->mostrarError();
+            }
+
+
+            if ($elementos instanceof AppError) {
+                return $elementos->mostrarError();
+            }
+
+            if ($seccion === 'pacientes') {
+                $pacientes = $elementos;
+                $contenido = __DIR__ . "/../../pacientes/view/index.phtml";
+            } elseif ($seccion === 'empleados') {
+                $elementos = new Paginador($elementos, 1, 20);
+                $contenido = __DIR__ . "/../../empleados/view/index.phtml";
+            } else {
+                print_r($elementos);
+            }
+
+            return require __DIR__ . "/../../view/pagina.phtml";
+        }
+    }
+}
